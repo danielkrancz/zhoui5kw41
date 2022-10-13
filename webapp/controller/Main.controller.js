9bcf05f3484bbeddfac336fcc665032f74d3b1d2
sapux00 clouddnagmbhgmail.onmicrosoft.com <sapux00@clouddnagmbhgmail.onmicrosoft.com>
2022-10-13 09:40:21 +0000

    6.1 Tabelle einbauen

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("at.clouddna.training00.zhoui5.controller.Main", {
+
+            onInit: function(){
+
+            }
+            
+        });
+    });
